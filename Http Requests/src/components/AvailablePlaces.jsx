cf9022f9bed1f68021354d1c0b5262728588b7b5
RehanGeorge <rehan.george@gmail.com>
2024-04-04 21:12:40 +0530

    Add async fetch

--- Http Requests/src/components/AvailablePlaces.jsx
+++ Http Requests/src/components/AvailablePlaces.jsx
@@ -3,18 +3,33 @@ import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/places')
+  //   .then(response => response.json())
+  //   .then(data => setAvailablePlaces(data.places));
+  // }, []);
+
   useEffect(() => {
-    fetch('http://localhost:3000/places')
-    .then(response => response.json())
-    .then(data => setAvailablePlaces(data.places));
+    async function fetchPlaces() {
+      setIsFetching(true);
+      const response = await fetch('http://localhost:3000/places');
+      const data = await response.json();
+      setAvailablePlaces(data.places);
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
   }, []);
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
