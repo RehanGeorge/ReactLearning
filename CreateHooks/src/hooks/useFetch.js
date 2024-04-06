eeb71eeb2f94db7f80eefba806a00324dad8c83c
RehanGeorge <rehan.george@gmail.com>
2024-04-06 19:51:33 +0530

    Complete app

--- CreateHooks/src/hooks/useFetch.js
+++ CreateHooks/src/hooks/useFetch.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 export function useFetch(fetchFn, initialValue) {
   const [isFetching, setIsFetching] = useState();
