6e1b3296a4abb587aa5bb7556fcf64240fc42350
RehanGeorge <rehan.george@gmail.com>
2024-04-10 22:02:33 +0530

    Add custom validation

--- React Forms/src/components/Signup.jsx
+++ React Forms/src/components/Signup.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 
 export default function Signup() {
+  const [passwordsAreNotEqual, setPasswordsAreNotEqual] = useState(false);
+
     function handleSubmit(event) {
         event.preventDefault();
 
@@ -7,6 +10,12 @@ export default function Signup() {
         const acquisitionChannel = fd.getAll('acquisition');
         const data = Object.fromEntries(fd.entries());
         data.acquisition = acquisitionChannel;
+
+        if (data.password !== data['confirm-password']) {
+            setPasswordsAreNotEqual(true);
+            return;
+        }
+
         console.log(data);
 
         // event.target.reset;
@@ -20,13 +29,13 @@ export default function Signup() {
   
         <div className="control">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" required />
         </div>
   
         <div className="control-row">
           <div className="control">
             <label htmlFor="password">Password</label>
-            <input id="password" type="password" name="password" />
+            <input id="password" type="password" name="password" required minLength={6}/>
           </div>
   
           <div className="control">
@@ -36,6 +45,7 @@ export default function Signup() {
               type="password"
               name="confirm-password"
             />
+            <div className="control-error">{passwordsAreNotEqual && <p>Passwords do not match.</p>}</div>
           </div>
         </div>
   
