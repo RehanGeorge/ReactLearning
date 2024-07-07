56ee3892df7ec893425542affc5355a242159e2d
RehanGeorge <rehan.george@gmail.com>
2024-07-07 17:42:43 +0530

    Conditionally display 'Go to Checkout'

--- Food Order/src/components/Cart.jsx
+++ Food Order/src/components/Cart.jsx
@@ -35,7 +35,7 @@ export default function Cart() {
         <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
         <p className="modal-actions">
             <Button textOnly onClick={handleCloseCart}>Close</Button>
-            <Button onClick={handleCloseCart}>Go To Checkout</Button>
+            {cartCtx.items.length > 0 && <Button onClick={handleCloseCart}>Go To Checkout</Button>}
         </p>
     </Modal>
 }
