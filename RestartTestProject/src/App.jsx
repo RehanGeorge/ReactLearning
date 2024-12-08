e2acf9bae69272a5870979cc924b69f51728929c
Rehan George <rehan.george@gmail.com>
2024-12-08 18:41:25 +0530

    Add Result component

--- RestartTestProject/src/App.jsx
+++ RestartTestProject/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import InvestmentForm from "./components/InvestmentForm"
+import Result from "./components/Result"
 
 function App() {
   return (
     <>
       <Header />
       <InvestmentForm />
+      <Result />
     </>
   )
 }
