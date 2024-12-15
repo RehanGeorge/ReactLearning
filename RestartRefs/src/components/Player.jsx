44b1f3baad5a78d0cb158868a24888e7818de381
Rehan George <rehan.george@gmail.com>
2024-12-15 18:38:04 +0530

    Add ref

--- RestartRefs/src/components/Player.jsx
+++ RestartRefs/src/components/Player.jsx
@@ -1,10 +1,19 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const [userName, setUserName] = useState("unknown entity");
+  const nameInput = useRef();
+  
+  function setNameHandler() {
+    setUserName(nameInput.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {userName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={nameInput} />
+        <button onClick={setNameHandler}>Set Name</button>
       </p>
     </section>
   );
