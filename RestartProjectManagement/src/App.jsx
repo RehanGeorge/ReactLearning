630a17ee999f250c42470ed9ddc102ad5eda1592
Rehan George <rehan.george@gmail.com>
2024-12-22 19:07:10 +0530

    Add variable content

--- RestartProjectManagement/src/App.jsx
+++ RestartProjectManagement/src/App.jsx
@@ -1,15 +1,37 @@
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
+import { useState } from "react";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject() {
+    setProjectsState((prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    }))
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <>
       <div className="h-screen my-8 flex gap-8">
-        <Sidebar />
-        <NewProject />
+        <Sidebar onStartAddProject={handleStartAddProject} />
+        {content}
       </div>
-      <NoProjectSelected />
     </>
   );
 }
