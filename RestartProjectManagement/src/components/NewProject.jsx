397cdcc2e36b765908ba8157f5b7dde591ef8df4
Rehan George <rehan.george@gmail.com>
2024-12-24 18:32:28 +0530

    Add Modal

--- RestartProjectManagement/src/components/NewProject.jsx
+++ RestartProjectManagement/src/components/NewProject.jsx
@@ -1,8 +1,11 @@
 import { useRef } from "react";
 
 import Input from "./Input";
+import Modal from "./Modal";
 
 export default function NewProject({ onAdd }) {
+    const modal = useRef();
+
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -12,7 +15,13 @@ export default function NewProject({ onAdd }) {
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
-        // Validation...
+        // Validation of Input
+        if (enteredTitle.trim().length === 0 || 
+        enteredDescription.trim().length === 0 || 
+        enteredDueDate.trim().length === 0) {
+            modal.current.open();
+            return;
+        }
 
         onAdd({
             title: enteredTitle,
@@ -22,18 +31,25 @@ export default function NewProject({ onAdd }) {
     }
 
     return (
-        <div className="w-[35rem] mt-16">
-            <menu className="flex items-center justify-end gap-4 my-4">
-                <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-                <li><button 
-                        className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-                        onClick={handleSave}>Save</button></li>
-            </menu>
-            <div>
-                <Input ref={title} label="Title" />
-                <Input ref={description} label="Description" textarea />
-                <Input ref={dueDate} label="Due Date" />
+        <>
+            <Modal ref={modal} caption="Okay">
+                <h2>Invalid input</h2>
+                <p>Oops... looks like you forgot to enter a value.</p>
+                <p>Please make sure you provide a valid value for every input field.</p>
+            </Modal>
+            <div className="w-[35rem] mt-16">
+                <menu className="flex items-center justify-end gap-4 my-4">
+                    <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+                    <li><button 
+                            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+                            onClick={handleSave}>Save</button></li>
+                </menu>
+                <div>
+                    <Input ref={title} label="Title" />
+                    <Input ref={description} label="Description" textarea />
+                    <Input ref={dueDate} label="Due Date" />
+                </div>
             </div>
-        </div>
+        </>
     );
 }
