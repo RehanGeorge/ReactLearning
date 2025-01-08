9bf8b6de0b48614a4391e08bb6030f90b80b1f0a
Rehan George <rehan.george@gmail.com>
2025-01-08 20:52:19 +0530

    Add Quiz questions

--- RestartQuizApp/src/components/Quiz.jsx
+++ RestartQuizApp/src/components/Quiz.jsx
@@ -1,16 +1,30 @@
-import { useState } from "react"
+import QUESTIONS from "../../questions";
+
+import { useState } from "react";
 
 export default function Quiz() {
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
     const [userAnswers, setUserAnswers] = useState([]);
 
+    const activeQuestionIndex = userAnswers.length;
+
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+    }
+
     return (
-        <div>
-            <p>Currently active question</p>
-            <p>Answer 1</p>
-            <p>Answer 2</p>
-            <p>Answer 3</p>
-            <p>Answer 4</p>
+        <div id="quiz">
+            <div id="question">
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id="answers">
+                    {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+                        <li key={index} className="answer">
+                            <button onClick={() => handleSelectAnswer(answer)}>
+                                {answer}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
