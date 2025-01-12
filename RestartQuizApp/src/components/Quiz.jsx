1ba7954be2c876b03b60c5db8b5022ab334a1f64
Rehan George <rehan.george@gmail.com>
2025-01-12 18:23:19 +0530

    Simplify components

--- RestartQuizApp/src/components/Quiz.jsx
+++ RestartQuizApp/src/components/Quiz.jsx
@@ -4,28 +4,14 @@ import QUESTIONS from "../../questions";
 import Question from "./Question";
 
 export default function Quiz() {
-    const [answerState, setAnswerState] = useState('');
     const [userAnswers, setUserAnswers] = useState([]);
 
-    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
+    const activeQuestionIndex = userAnswers.length;
     const quizIsOver = activeQuestionIndex === QUESTIONS.length;
 
     const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
-        setAnswerState('answered');
         setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
-
-        setTimeout(() => {
-            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-                setAnswerState('correct');
-            } else {
-                setAnswerState('wrong');
-            }
-
-            setTimeout(() => {
-                setAnswerState('');
-            }, 2000);
-        }, 1000);
-    }, [activeQuestionIndex]);
+    }, []);
 
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
@@ -42,10 +28,7 @@ export default function Quiz() {
         <div id="quiz">
             <Question
              key={activeQuestionIndex}
-             questionText={QUESTIONS[activeQuestionIndex].text}
-             answers={QUESTIONS[activeQuestionIndex].answers}
-             answerState={answerState}
-             selectedAnswer={userAnswers[activeQuestionIndex]}
+             index={activeQuestionIndex}
              onSelectAnswer={handleSelectAnswer}
              onSkipAnswer={handleSkipAnswer}
              />
