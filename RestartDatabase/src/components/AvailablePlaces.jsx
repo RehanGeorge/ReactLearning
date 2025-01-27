3e82819358c0fe66097de463fe3fa9d3d78e811e
Rehan George <rehan.george@gmail.com>
2025-01-27 19:36:15 +0530

    Add error handling

--- RestartDatabase/src/components/AvailablePlaces.jsx
+++ RestartDatabase/src/components/AvailablePlaces.jsx
@@ -5,13 +5,24 @@ import Places from './Places.jsx';
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch places');
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError(error);
+      }
+
       setIsFetching(false);
     }
 
