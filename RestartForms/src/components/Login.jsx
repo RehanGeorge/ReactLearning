7697b03ad2a106cfe7a770df089bc6a880166966
Rehan George <rehan.george@gmail.com>
2025-02-11 10:18:00 +0530

    Use Refs instead

--- RestartForms/src/components/Login.jsx
+++ RestartForms/src/components/Login.jsx
@@ -1,22 +1,16 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 export default function Login() {
-  const [enteredValues, setEnteredValues] = useState({
-    email: "",
-    password: "",
-  });
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-  }
 
-  function handleInputChange(identifier, value) {
-    setEnteredValues((prevValues) => {
-      return {
-        ...prevValues,
-        [identifier]: value
-      }
-    })
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    
+    console.log(enteredEmail, enteredPassword);
   }
 
   return (
@@ -26,12 +20,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onChange={(event) => handleInputChange('email', event.target.value)} value={enteredValues.email}/>
+          <input id="email" type="email" name="email" ref={emailInputRef}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" onChange={(event) => handleInputChange('password', event.target.value)} value={enteredValues.password}/>
+          <input id="password" type="password" name="password" ref={passwordInputRef}/>
         </div>
       </div>
 
