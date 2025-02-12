10c1bee8c300acb112779ed98d5c23c5b00d5a61
Rehan George <rehan.george@gmail.com>
2025-02-12 21:19:25 +0530

    Add email validation

--- RestartForms/src/components/StateLogin.jsx
+++ RestartForms/src/components/StateLogin.jsx
@@ -6,6 +6,8 @@ export default function Login() {
     password: "",
   });
 
+  const emailIsInvalid = !enteredValues.email.includes('@')
+
   function handleSubmit(event) {
     event.preventDefault();
   }
@@ -27,6 +29,7 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" onChange={(event) => handleInputChange('email', event.target.value)} value={enteredValues.email}/>
+        <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
         </div>
 
         <div className="control no-margin">
