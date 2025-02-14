1864b73471337e47f240625d1d59b1229a1a372f
Rehan George <rehan.george@gmail.com>
2025-02-14 18:53:24 +0530

    Add validation

--- RestartForms/src/components/Signup.jsx
+++ RestartForms/src/components/Signup.jsx
@@ -20,13 +20,13 @@ export default function Signup() {
   
         <div className="control">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" required />
         </div>
   
         <div className="control-row">
           <div className="control">
             <label htmlFor="password">Password</label>
-            <input id="password" type="password" name="password" />
+            <input id="password" type="password" name="password" required minLength={6}/>
           </div>
   
           <div className="control">
@@ -35,6 +35,7 @@ export default function Signup() {
               id="confirm-password"
               type="password"
               name="confirm-password"
+              required
             />
           </div>
         </div>
@@ -94,7 +95,7 @@ export default function Signup() {
   
         <div className="control">
           <label htmlFor="terms-and-conditions">
-            <input type="checkbox" id="terms-and-conditions" name="terms" />I
+            <input type="checkbox" id="terms-and-conditions" name="terms" required/>I
             agree to the terms and conditions
           </label>
         </div>
