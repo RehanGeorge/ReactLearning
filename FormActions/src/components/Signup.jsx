869a35f973bdb3ac495f5fa40139ac49e3d8fbb8
Rehan George <rehan.george@gmail.com>
2025-02-17 16:18:12 +0530

    Add useActionState

--- FormActions/src/components/Signup.jsx
+++ FormActions/src/components/Signup.jsx
@@ -4,7 +4,8 @@ import { isEmail, isNotEmpty, isEqualToOtherValue, hasMinLength } from '../util/
 
 export default function Signup() {
 
-  function signupAction(formData) {
+  // When calling using useActionState, the formData object should be the second argument
+  function signupAction(prevFormState, formData) {
     const email = formData.get('email');
     const password = formData.get('password');
     const confirmPassword = formData.get('confirm-password');
@@ -51,8 +52,10 @@ export default function Signup() {
     return { errors: null}
   }
 
+  const [formState, formAction] = useActionState(signupAction, { errors: null });
+
   return (
-    <form action={signupAction}>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -136,6 +139,14 @@ export default function Signup() {
           agree to the terms and conditions
         </label>
       </div>
+
+      {formState.errors && (
+        <ul className="error">
+          {formState.errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+        )}
 
       <p className="form-actions">
         <button type="reset" className="button button-flat">
