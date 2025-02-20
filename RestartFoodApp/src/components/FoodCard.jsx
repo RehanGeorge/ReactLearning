52de448b4fd7734153cc6208757579e35db1ffed
Rehan George <rehan.george@gmail.com>
2025-02-20 14:10:56 +0530

    Initiate cart functionality

--- RestartFoodApp/src/components/FoodCard.jsx
+++ RestartFoodApp/src/components/FoodCard.jsx
@@ -1,11 +1,15 @@
 export default function FoodCard({ meal }) {
+  function handleAddToCart() {
+    console.log(`Adding ${meal.id} to cart`);
+  }
+
   return (
     <div className="meal-item">
       <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
       <h3>{meal.name}</h3>
       <p className="meal-item-price">${meal.price}</p>
       <p className="meal-item-description">{meal.description}</p>
-      <button className="button">Add to Cart</button>
+      <button className="button" onClick={handleAddToCart}>Add to Cart</button>
     </div>
   );
 }
