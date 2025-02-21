c65040cd39135357c64a57013b88599615ee27fc
Rehan George <rehan.george@gmail.com>
2025-02-21 19:32:10 +0530

    Add header

--- RestartFoodApp/src/App.jsx
+++ RestartFoodApp/src/App.jsx
@@ -1,8 +1,12 @@
 import Meals from "./components/Meals";
+import logoImg from "./assets/logo.jpg";
 
 function App() {
   return (
     <>
+      <header id="main-header">
+        <h1 id="title"><img src={logoImg} alt="Burger" />ReactFood</h1>
+      </header>
       <Meals />
     </>
   );
