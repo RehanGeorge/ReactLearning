d8f4101350e8d9969f15bc41723e5a9d40527330
Rehan George <rehan.george@gmail.com>
2025-02-23 20:33:55 +0530

    Add dummy UI

--- RestartFoodApp/src/components/CartModal.jsx
+++ RestartFoodApp/src/components/CartModal.jsx
@@ -12,7 +12,27 @@ export default function CartModal({ ref }) {
 
     return (
         <dialog className="modal" ref={dialogModal}>
-            <div className="modal-content">Hi</div>
+            <div className="cart">
+                <h2>Your Cart</h2>
+                <ul className="cart-items">
+                    <li>
+                        <div className="cart-item">
+                            <p>Item 1</p>
+                            <div className="cart-item-actions">
+                                <button>-</button>
+                                <span>1</span>
+                                <button>+</button>
+                            </div>
+                        </div>
+                    </li>
+                    <p>Item 2</p>
+                    <p>Item 3</p>
+                </ul>
+                <div className="modal-actions">
+                    <button className="text-button" onClick={() => dialogModal.current.close()}>Close</button>
+                    <button className="button">Go to Checkout</button>
+                </div>
+            </div>
         </dialog>
     )
 }
