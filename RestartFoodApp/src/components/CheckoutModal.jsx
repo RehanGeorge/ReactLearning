aed83a50cd89e3560c8ed5f15b2f49a14fa45535
Rehan George <rehan.george@gmail.com>
2025-02-26 19:49:13 +0530

    Add post action for customer data

--- RestartFoodApp/src/components/CheckoutModal.jsx
+++ RestartFoodApp/src/components/CheckoutModal.jsx
@@ -11,44 +11,49 @@ export default function CheckoutModal({ ref }) {
         close: () => dialogModal.current.close()
     }));
 
-    function handleSubmit() {
+    function handleSubmit(event) {
+        event.preventDefault();
+        customerData = new FormData(event.target);
+        console.log(customerData);
         dialogModal.current.close();
         successModal.current.open();
     }
 
     return (
         <>
-            <dialog className="modal" ref={dialogModal}>
-                <h2>Checkout</h2>
-                <div className="control">
+            <form onSubmit={handleSubmit}>
+                <dialog className="modal" ref={dialogModal}>
+                    <h2>Checkout</h2>
                     <div className="control">
-                        <label htmlFor="name">Full Name</label>
-                        <input type="text" id="name" name="name" required />
-                    </div>
-                    <div className="control">
-                        <label htmlFor="email">E-mail Address</label>
-                        <input type="email" id="email" name="email" required />
-                    </div>
-                    <div className="control">
-                        <label htmlFor="address">Street</label>
-                        <input id="address" name="address" required></input>
-                    </div>
-                    <div className="control-row">
                         <div className="control">
-                            <label htmlFor="postal">Postal Code</label>
-                            <input id="postal" name="postal" required></input>
+                            <label htmlFor="name">Full Name</label>
+                            <input type="text" id="name" name="name" required />
                         </div>
                         <div className="control">
-                            <label htmlFor="city">City</label>
-                            <input id="city" name="city" required></input>
+                            <label htmlFor="email">E-mail Address</label>
+                            <input type="email" id="email" name="email" required />
+                        </div>
+                        <div className="control">
+                            <label htmlFor="address">Street</label>
+                            <input id="street" name="street" required></input>
+                        </div>
+                        <div className="control-row">
+                            <div className="control">
+                                <label htmlFor="postal">Postal Code</label>
+                                <input id="postal" name="postal" required></input>
+                            </div>
+                            <div className="control">
+                                <label htmlFor="city">City</label>
+                                <input id="city" name="city" required></input>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="modal-actions">
-                    <button className="text-button" onClick={() => dialogModal.current.close()}>Close</button>
-                    <button className="button" onClick={handleSubmit}>Submit Order</button>
-                </div>
-            </dialog>
+                    <div className="modal-actions">
+                        <button className="text-button" onClick={() => dialogModal.current.close()}>Close</button>
+                        <button className="button" type="submit">Submit Order</button>
+                    </div>
+                </dialog>
+            </form>
             <SubmitModal ref={successModal}/>
         </>
     )
