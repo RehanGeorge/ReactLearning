bd721b9d8dd3f1c9da29ae0dfc13afd022c5e137
Rehan George <rehan.george@gmail.com>
2025-03-03 19:00:09 +0530

    Update hook in meals

--- RestartFoodApp/src/components/Meals.jsx
+++ RestartFoodApp/src/components/Meals.jsx
@@ -1,41 +1,18 @@
 import FoodCard from "./FoodCard";
+import useHttp from "../hooks/useHttp";
 
-import { useEffect, useState } from "react"
+const requestConfig = {};
 
 export default function Meals() {
-    const [meals, setMeals] = useState([]);
-    const [isFetching, setIsFetching] = useState(false);
-    const [error, setError] = useState();
-
-    async function fetchMeals() {
-        try {
-            setIsFetching(true);
-            const response = await fetch('http://localhost:3000/meals');
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const data = await response.json();
-            setMeals(data);
-        } catch (error) {
-            setError(error.message || 'Failed to fetch data.');
-            console.log(error);
-        }
-        setIsFetching(false);
-    }
-
-    useEffect(() => {
-        fetchMeals();
-    }, []);
-
+    const {data: meals, isLoading: isFetching, error} = useHttp("http://localhost:3000/meals", requestConfig, []);
+   
     return (
-        <>
-        {isFetching && <p>Fetching</p>}
-        {error && <p>{error}</p>}
-        {!isFetching && <div id="meals">{meals.map((meal) => {
-            return (
-                <FoodCard key={meal.id} meal={meal} />
-            )
-        })}</div>}
-        </>
+        <ul id="meals">
+            {!isFetching && <div id="meals">{meals.map((meal) => {
+                return (
+                    <FoodCard key={meal.id} meal={meal} />
+                )
+            })}</div>}
+        </ul>
     )
 }
