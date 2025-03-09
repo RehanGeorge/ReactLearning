d2ac83094fde3a9ecf997a45b68123ea734e1fb8
Rehan George <rehan.george@gmail.com>
2025-03-09 18:57:09 +0530

    Add additional state

--- RestartReduxApp/src/components/Counter.js
+++ RestartReduxApp/src/components/Counter.js
@@ -5,23 +5,31 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const displayCounter = useSelector(state => state.showCounter);
 
   const handleIncrement = () => {
     dispatch({ type: 'increment' });
+  }
+
+  const handleIncrease = () => {
+    dispatch({ type: 'increase', amount: 5 });
   }
 
   const handleDecrement = () => {
     dispatch({ type: 'decrement' });
   }
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {displayCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleIncrease}>Increment by 5</button>
         <button onClick={handleDecrement}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
