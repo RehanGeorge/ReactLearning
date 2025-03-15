485fbd8118c7baf906d17414402d55570c470a78
Rehan George <rehan.george@gmail.com>
2025-03-15 18:07:32 +0530

    Add backend

---
+++ AdvancedRedux/src/backend/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(3001, () => {
+    console.log('Server is running on http://localhost:3001');
+});
