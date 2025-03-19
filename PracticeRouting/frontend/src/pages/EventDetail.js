7f38eb16f015c9e79356dc30883fa5ff56ce65ca
Rehan George <rehan.george@gmail.com>
2025-03-19 15:00:46 +0530

    Use params

--- PracticeRouting/frontend/src/pages/EventDetail.js
+++ PracticeRouting/frontend/src/pages/EventDetail.js
@@ -1,12 +1,13 @@
-import { useLocation, Link } from "react-router-dom";
+import { useLocation, Link, useParams } from "react-router-dom";
 
 export default function EventDetailsPage() {
+    const idParam = useParams().eventId;
     const location = useLocation();
     const { description } = location.state;
 
     return (
         <div>
-            <h1>This is the Event Details Page.</h1>
+            <h1>This is the Event Details Page for {idParam}</h1>
             <p>{description}</p>
             <Link to=".." relative="path">Back</Link>
         </div>
