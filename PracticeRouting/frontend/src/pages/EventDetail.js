5e93d37fd2d02be265a43ce7cb37915817f1cdb9
Rehan George <rehan.george@gmail.com>
2025-03-19 12:48:31 +0530

    Add pages

---
+++ PracticeRouting/frontend/src/pages/EventDetail.js
@@ -0,0 +1,7 @@
+export default function EventDetailsPage() {
+    return (
+        <div>
+            <h1>This is the Event Details Page.</h1>
+        </div>
+    )
+}
---
+++ PracticeRouting/frontend/src/pages/NewEvent.js
@@ -0,0 +1,7 @@
+export default function NewEventPage() {
+    return (
+        <div>
+            <h1>This is the New Event Page.</h1>
+        </div>
+    )
+}
