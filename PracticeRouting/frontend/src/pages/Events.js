76dabf0f32f50c3b87a38ffa57b67434877cb14e
Rehan George <rehan.george@gmail.com>
2025-03-20 14:18:41 +0530

    Add Events page

--- PracticeRouting/frontend/src/pages/Events.js
+++ PracticeRouting/frontend/src/pages/Events.js
@@ -1,23 +1,37 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-export default function EventsPage() {
-    const DUMMY_EVENTS = [
-        { id: 'e1', title: 'Programming for Everyone', description: 'This is a first event!', date: '2022-03-18' },
-        { id: 'e2', title: 'Networking for Introverts', description: 'This is the second event!', date: '2022-04-10' },
-        { id: 'e3', title: 'Networking for Extroverts', description: 'This is the third event!', date: '2022-05-15' },
-        { id: 'e4', title: 'Networking for Ambiverts', description: 'This is the fourth event!', date: '2022-06-20' }
-    ]
+import EventsList from '../components/EventsList';
 
-    return (
-        <div>
-            <h1>Events Page</h1>
-            <ul>
-                {DUMMY_EVENTS.map((event) => (
-                    <li key={event.id}>
-                        <Link to={`/events/${event.id}`}>{event.title}</Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+function EventsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      const response = await fetch('http://localhost:8080/events');
+
+      if (!response.ok) {
+        setError('Fetching events failed.');
+      } else {
+        const resData = await response.json();
+        setFetchedEvents(resData.events);
+      }
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
+  return (
+    <>
+      <div style={{ textAlign: 'center' }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+    </>
+  );
 }
+
+export default EventsPage;
