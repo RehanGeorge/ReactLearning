583bbafe7382b226a518b9b7135206bba399c1ea
Rehan George <rehan.george@gmail.com>
2025-03-22 18:58:39 +0530

    Add feedback to add event when submitting

--- PracticeRouting/frontend/src/components/EventForm.js
+++ PracticeRouting/frontend/src/components/EventForm.js
@@ -1,8 +1,12 @@
-import { Form } from 'react-router-dom';
+import { Form, useNavigation } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === 'submitting';
+
     function cancelHandler() {
 
   }
@@ -26,10 +30,10 @@ function EventForm({ method, event }) {
         <textarea id="description" name="description" rows="5" required defaultValue={event ? event.description : ''} />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>{isSubmitting ? 'Submitting...' : 'Save'}</button>
       </div>
     </Form>
   );
