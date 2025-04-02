d286a139ae9ad591f3a4779a70bda95149396233
Rehan George <rehan.george@gmail.com>
2025-04-02 19:25:53 +0530

    Add deletion logic

--- TanStack/src/components/Events/EventDetails.jsx
+++ TanStack/src/components/Events/EventDetails.jsx
@@ -1,17 +1,31 @@
-import { Link, Outlet, useParams } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
+import { Link, Outlet, useParams, useNavigate } from 'react-router-dom';
+import { useQuery, useMutation } from '@tanstack/react-query';
+import { deleteEvent } from '../../util/http.js';
 
 import Header from '../Header.jsx';
-import { fetchEvent } from '../../util/http.js';
+import { fetchEvent, queryClient } from '../../util/http.js';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EventDetails() {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const { data, isPending, isError, error } = useQuery({
     queryKey: ['events', id],
     queryFn: ({ signal }) => fetchEvent({ signal, id }),
-  })
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['events'] });
+      navigate('/events');
+    }
+  });
+
+  function handleDelete() {
+    mutate({ id: id });
+  }
 
   let content;
 
@@ -43,7 +57,7 @@ export default function EventDetails() {
         <header>
             <h1>{data.title}</h1>
             <nav>
-              <button>Delete</button>
+              <button onClick={handleDelete}>Delete</button>
               <Link to="edit">Edit</Link>
             </nav>
           </header>
